7d10a607bb3d7e31aa99c5d8c90fbcc9a5ced838
xorik <xorik@users.noreply.github.com>
2021-05-20 11:40:39 +0200

    Backdrop bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h3 class="text-3xl font-bold text-primary p-5">Hello Vite</h3>
+
+  <div class="w-40 h-40 bg-blue-400 bg-opacity-50 backdrop-filter backdrop-blur-md relative -top-15 left-5"></div>
 </template>
 
 
